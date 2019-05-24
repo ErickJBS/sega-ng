f979f4daf3ebcb500896eb0469f9eb8de8f38e9c
ErickB <a311008@uach.mx>
2019-05-24 00:53:01 -0600

    login fix

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,21 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { environment } from '../../environments/environment';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map, first } from 'rxjs/operators';
-import { User } from '@models/user';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  currentUserSubject: BehaviorSubject<User>;
-  user: Observable<User>;
   token: string;
 
   constructor(private router: Router, private http: HttpClient) {
-    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
-    this.user = this.currentUserSubject.asObservable();
   }
 
   signIn(user: string, password: string) {
@@ -24,6 +18,7 @@ export class AuthService {
     return this.http.post<any>(url, { user, password })
       .pipe(map(data => {
         if (data && data.token) {
+          data.user = user;
           this.setToken(data);
         }
         return data;
@@ -34,12 +29,12 @@ export class AuthService {
     // remove user from local storage to log user out
     localStorage.removeItem('currentUser');
     this.token = null;
-    this.currentUserSubject.next(null);
     this.router.navigate(['/login']);
   }
 
   getUser() {
-    return this.user.pipe(first()).toPromise();
+    const data: any = this.getToken();
+    return data.user;
   }
 
   getToken() {
@@ -48,7 +43,8 @@ export class AuthService {
     }
     else {
       if (localStorage.getItem('currentUser')) {
-        return JSON.parse(localStorage.getItem('currentUser'));
+        this.token = JSON.parse(localStorage.getItem('currentUser'));
+        return this.token;
       }
       return null;
     }
