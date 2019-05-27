191424b15fb7477d7ef06f96f08faa7e6f50dbf7
ErickB <a311008@uach.mx>
2019-05-26 23:42:23 -0600

    production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: false,
+  apiServer: 'http://ec2-52-14-207-216.us-east-2.compute.amazonaws.com:8000'
+  //apiServer: 'http://localhost:8000'
 };
